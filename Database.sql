effbc32cc5e811375da3dcc65bc2fc6f50edf353
linhtp2000 <linhtp157@gmail.com>
2021-05-15 09:30:03 +0700

    update01+database

---
+++ Database.sql
@@ -0,0 +1,32 @@
+﻿use QuanLyDinhDuong
+
+insert into Thu(Id, Name) values(1,'Hai');
+insert into Thu(Id, Name) values(2,'Ba');
+insert into Thu(Id, Name) values(3,'Tư');
+insert into Thu(Id, Name) values(4,'Năm');
+insert into Thu(Id, Name) values(5,'Sáu');
+insert into Thu(Id, Name) values(6,'Bảy');
+insert into Thu(Id, Name) values(7,'Chủ Nhật');
+
+insert into Buoi(Id, Name) values(1,'Sáng');
+insert into Buoi(Id, Name) values(2,'Trưa');
+insert into Buoi(Id, Name) values(3,'Chiều');
+
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Gạo nếp cái',346,75,9,2,'Chất xơ 0.6g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(2,'Gạo tẻ',344,76,8,1,'Chất xơ 0.4g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(3,'Bắp tươi',196,40,4,2,'Chất xơ 1.2g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(4,'Bánh bao',219,48,6,1,'Chất xơ 0.5g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(5,'Bánh tráng mỏng',333,79,4,0,'Chất xơ 0.5g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(6,'Bánh đúc',52,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(7,'Bánh mì',249,53,8,1,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(8,'Bánh phở',141,32,3,2,'Chất xơ 0g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(9,'Bún',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(10,'Củ sắn',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(11,'Củ từ',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(12,'Khoai lang',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(13,'Khoai lang nghệ',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Bột sắn dây',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Miến dong',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Khoai tây chiên',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Cùi dừa già',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
+insert into MonAn(Id, Name,Calo,Carb,Protein,Fat,ThongTin,HinhAnh) values(1,'Gạo nếp cái',346,75,9,2,'Chất xơ 0.9g, Cholesterol 0mg','');
